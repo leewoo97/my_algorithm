235d7e5930eb0e3aaec15e6782cc5a2308dade8e
Woo Jin-Lee <138551127+SNOWMAN-O3O@users.noreply.github.com>
2024-02-10 13:30:22 +0900

    [D5] Title: 촛불 이벤트, Time: 748 ms, Memory: 96,948 KB -BaekjoonHub

---
+++ SWEA/D5/9843. 촛불 이벤트/README.md
@@ -0,0 +1,15 @@
+# [D5] 촛불 이벤트 - 9843 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXGBKzuaPOoDFAXR) 
+
+### 성능 요약
+
+메모리: 96,948 KB, 시간: 748 ms, 코드길이: 1,192 Bytes
+
+### 제출 일자
+
+2024-02-10 13:30
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D5/9843. 촛불 이벤트/촛불 이벤트.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static Long N; // 양초의 총 갯수
+	static Long L; // 배열의 가장 첫번째
+	static Long R; // 배열의 가장 끝
+	
+	public static void main(String[] args) throws IOException {
+		st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(st.nextToken());
+
+		for (int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			N = Long.parseLong(st.nextToken()); // 양초의 총 갯수 //10의 18승이라 int범위 벗어날듯
+			L = 1L;
+			R = 2000000000L;
+			
+			System.out.println("#" + tc + " " + binarySearch());
+		}
+	}
+
+	public static Long binarySearch() {// 이분탐색
+		while (L <= R) {
+			Long mid = (L + R) / 2;
+			Long value = mathResult(mid);
+			
+			if(value<N) {
+				L = mid+1L;
+			}else if(value>N) {
+				R = mid-1L;
+			}else if(value.equals(N)) {
+				return mid;
+			}
+//			System.out.println(value);
+		}
+		return -1L;
+	}
+	
+	public static Long mathResult(Long mid) { //K를 대입한 수식값
+		return (mid*(mid+1))/2;
+	}
+}
