fb2f1de7de32d038befb39ac829af9fe3b373c3f
Woo Jin-Lee <138551127+leewoo97@users.noreply.github.com>
2024-09-10 16:38:43 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 3, Time: 1512 ms, Memory: 96852 KB -BaekjoonHub

--- 백준/Silver/24481. 알고리즘 수업 － 깊이 우선 탐색 3/알고리즘 수업 － 깊이 우선 탐색 3.java
+++ 백준/Silver/24481. 알고리즘 수업 － 깊이 우선 탐색 3/알고리즘 수업 － 깊이 우선 탐색 3.java
@@ -2,85 +2,72 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-	
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
-	static List<List<Integer>> node; //무방향 그래프
-	static boolean[] visited; //DFS 방문배열
-	static int N; //정점의 수
-	static int M; //간선의 수
-	static int R; //시작 정점
-	static int[] result; //정답 배열
+	static int N; //정점 수
+	static int M; //간선 수
+	static List<List<Integer>> list;
+	static int[] visited;
 	
 	public static void main(String[] args) throws IOException {
 		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		int R = Integer.parseInt(st.nextToken()); //시작 정점
 		
-		N = Integer.parseInt(st.nextToken()); //정점의 수
-		M = Integer.parseInt(st.nextToken()); //간선의 수
-		R = Integer.parseInt(st.nextToken()); //시작 정점
-		
-		nodeSet();//양방향(무방향) 그래프 설정 및 초기화
-		visited = new boolean[N+1];//DFS 방문배열 설정 및 초기화
-		result = new int[N+1];//정답 배열 설정 및 초기화
-		
-		for(int i=0; i<=N; i++) {
-			result[i] = -1;
-		}
-		
+		listSet();
+		visitedSet();
 		DFS(R,0);
-		
-//		System.out.println(Arrays.toString(result));
-		
-		resultView();
-		
-		
-		
+		printVisited();
 	}
 	
-	public static void nodeSet() throws IOException { //양방향(무방향)그래프 설정
-		node = new ArrayList<>();
+	public static void listSet() throws IOException {
+		list = new ArrayList<List<Integer>>();
 		
 		for(int i=0; i<=N; i++) {
-			node.add(new ArrayList<>());
+			list.add(new ArrayList<Integer>());
 		}
 		
 		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			node.get(a).add(b);
-			node.get(b).add(a);
-			
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			list.get(u).add(v);
+			list.get(v).add(u);
 		}
 		
-		for(int i=1; i<=N; i++) {
-			Collections.sort(node.get(i));
+		for(int i=0; i<=N; i++) {
+			Collections.sort(list.get(i)); 
 		}
-		
 	}
 	
-	public static void DFS(int v,int depth) {//깊이 우선 탐색
-		visited[v] = true;
-		result[v] = depth;
+	public static void visitedSet() {
+		visited = new int[N+1];
+		for(int i=1; i<=N; i++) {
+			visited[i] = -1;
+		}
+	}
+	
+	public static void DFS(int v, int depth) {
+		visited[v] = depth;
 		
-		for(int i=0; i<node.get(v).size(); i++) {
-			if(!visited[node.get(v).get(i)]) {
-				DFS(node.get(v).get(i),depth+1);
+		for(int i=0; i<list.get(v).size(); i++) {
+			int nextV = list.get(v).get(i);
+			if(visited[nextV]==-1) {
+				DFS(nextV, depth+1);
 			}
 		}
-		
 	}
 	
-	public static void resultView() {//정답 배열 확인
+	public static void printVisited() {
 		for(int i=1; i<=N; i++) {
-			System.out.println(result[i]);
+			System.out.println(visited[i]);
 		}
 	}
-	
 }
