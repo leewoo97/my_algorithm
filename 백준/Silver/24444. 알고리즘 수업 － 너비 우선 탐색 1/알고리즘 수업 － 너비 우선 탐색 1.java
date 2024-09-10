dae350ac088d46438c6055c5b07a829746a133da
Woo Jin-Lee <138551127+leewoo97@users.noreply.github.com>
2024-09-10 17:17:37 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 1544 ms, Memory: 97744 KB -BaekjoonHub

--- 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -2,66 +2,80 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static ArrayList<ArrayList<Integer>> list;
+	static Queue<Integer> queue;
 	static int[] visited;
-	static int Count = 0;
+	static int visitedNum;
+	static int N; //정점의 수
+	static int M; //간선의 수
 	
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		queue = new LinkedList<Integer>();
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		int R = Integer.parseInt(st.nextToken());
-
-		visited = new int[N + 1];
-		Queue<Integer> q = new LinkedList<>();
-
-		ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-		for (int i = 0; i <= N; i++) {
-			ArrayList<Integer> list2 = new ArrayList<>();
-			list.add(list2);
-		}
-
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int temp = Integer.parseInt(st.nextToken());
-			int temp2 = Integer.parseInt(st.nextToken());
-
-			list.get(temp).add(temp2);
-			list.get(temp2).add(temp);
+		
+		visitedNum = 1;
+		listSet();
+		visitedSet();
+		BFS(R);
+		printVisited();
+	}
+	
+	public static void listSet() throws IOException {
+		list = new ArrayList<ArrayList<Integer>>();
+		for(int i=0; i<=N; i++) {
+			list.add(new ArrayList<Integer>());
 		}
 		
-		for(int i=0; i<=N;i++) {
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			list.get(u).add(v);
+			list.get(v).add(u);
+		}
+		
+		for(int i=0; i<=N; i++) {
 			Collections.sort(list.get(i));
 		}
-		// 2차원 리스트 생성완료 //이 위로는 건들지 말것.
-		// BFS이용
-
-		q.add(R);
-		visited[R] = ++Count;
-
-		while (!q.isEmpty()) { 
-			int v = q.poll(); 
-//			System.out.println(v); 
-			for (int i : list.get(v)) {
-//				System.out.println(i);
-				if (visited[i]==0) { //true
-					q.add(i); 
-					visited[i] = ++Count; 
+	}
+	
+	public static void visitedSet() {
+		visited = new int[N+1];
+	}
+	
+	public static void BFS(int v) {
+		queue.add(v);
+		visited[v] = visitedNum++;
+		
+		while(!queue.isEmpty()) {
+			v = queue.poll();
+			for(int i=0; i<list.get(v).size(); i++) {
+				int nextV = list.get(v).get(i);
+				if(visited[nextV]==0) {
+					queue.add(nextV);
+					visited[nextV] = visitedNum++;
 				}
 			}
 		}
-		for(int i=1;i<visited.length;i++) {
+		
+	}
+	
+	public static void printVisited() {
+		for(int i=1; i<=N; i++) {
 			System.out.println(visited[i]);
 		}
-
 	}
-
+	
 }
