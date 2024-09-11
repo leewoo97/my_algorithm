3bc047e7176c075ebfacf5ed23f0723dfdf31454
Woo Jin-Lee <138551127+leewoo97@users.noreply.github.com>
2024-09-11 11:13:39 +0900

    [Gold V] Title: 토마토, Time: 640 ms, Memory: 173260 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -8,94 +8,106 @@ import java.util.StringTokenizer;
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
-	static int[][] map;//토마토 상자
-	static int M; //상자의 가로 칸 수
-	static int N; //상자의 세로 칸 수
-	static Queue<Integer> queue; //BFS 큐
-	static boolean[][] visited; //BFS 방문배열
+	static int[][] map; //토마토가 들어있는 상자
+	static int M; //상자의 가로
+	static int N; //상자의 세로
 	static int[] dy = {0,1,0,-1};
 	static int[] dx = {1,0,-1,0};
-	static int max;//토마토가 다 익은 날짜.
+	static Queue<Integer> queue;
+	static boolean[][] visited;
+	static int zeroCount;
 	
 	public static void main(String[] args) throws IOException {
 		st = new StringTokenizer(br.readLine());
 		M = Integer.parseInt(st.nextToken());
 		N = Integer.parseInt(st.nextToken());
-		
-		queue = new LinkedList<>(); //BFS 큐 설정 및 초기화
-		visited = new boolean[N][M]; //BFS 방문배열 설정 및 초기화
-		max = 0; //날짜 설정 및 초기화
-		
+		zeroCount = 0;
+		queueSet();
+		visitedSet();
 		mapSet();
 //		mapView();
-		BFS(0);
-		mapCheck();
-		System.out.println(max);
-		
+		System.out.println(BFS());
 	}
 	
-	public static void mapSet() throws IOException {//맵 설정 및 초기화
-		map = new int[N][M]; //세로 가로
-		
-		for(int i=0;i<N;i++) {
+	public static void queueSet() {
+		queue = new LinkedList<Integer>();
+	}
+	
+	public static void visitedSet() {
+		visited = new boolean[N][M];
+	}
+	
+	public static void mapSet() throws IOException {
+		map = new int[N][M];
+		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=0; j<M; j++) {
-//				System.out.println("x좌표 : "+ i + " y좌표 : "+ j);
 				map[i][j] = Integer.parseInt(st.nextToken()); 
+//				System.out.print(map[i][j]);
 				if(map[i][j]==1) {
-					queue.add(i);//세로 좌표 저장
-					queue.add(j);//가로 좌표 저장
-					queue.add(0);//처음 깊이는 0이다.(깊이로 몇일차인지 파악한다.)
-//					System.out.println("y좌표 : "+ i + " x좌표 : "+ j);
+					queue.add(i);
+					queue.add(j);
+					queue.add(0); //깊이
+				}else {
+					zeroCount++;
 				}
 			}
+//			System.out.println();
 		}
 	}
 	
-	public static void mapView() {//맵 확인
-		
-		for(int i=0;i<N;i++) {
+	public static void mapView() {
+		for(int i=0; i<N; i++) {
 			for(int j=0; j<M; j++) {
-				System.out.print(map[i][j]+" "); 
+				System.out.print(map[i][j]+" ");
 			}
 			System.out.println();
 		}
 	}
 	
-	public static void mapCheck() {//BFS가 전부 끝난후, 안 익은 토마토가 있는지 검사한다. 있다면 max는 -1
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<M; j++) {
-				if(map[i][j]==0) {
-					max=-1;
-					return;
+	public static int BFS() {
+		if(zeroCount == 0) { //기저조건 만약 모든 토마토가 처음부터 다 익어있었다면 '0'을 반환
+			return 0; 
+		}
+		
+		int depth = 0;
+		int maxDepth = 0;
+		
+		while(!queue.isEmpty()) {
+			int y = queue.poll();
+			int x = queue.poll();
+			depth = queue.poll();
+			if(maxDepth<=depth) {
+				maxDepth = depth;
+			}
+			visited[y][x] = true;
+			for(int i=0; i<4; i++) {
+				int py = y+dy[i];
+				int px = x+dx[i];
+				if(py>=0&&py<N&&px>=0&&px<M&&!visited[py][px]&&map[py][px]==0) {
+					visited[py][px] = true; //방문처리
+					queue.add(py);
+					queue.add(px);
+					queue.add(depth+1);
 				}
 			}
+		}
+		
+		if(visitedFail()) {
+			return -1;
+		}else {
+			return maxDepth;
 		}
 	}
 	
-	public static void BFS(int depth) { // depth는 깊이
-		while(!queue.isEmpty()) {
-			int py = queue.poll(); //세로값 꺼냄
-			int px = queue.poll(); //가로값 꺼냄
-			depth = queue.poll(); //깊이로 몇일차인지 파악한다.
-			max = depth;
-//			System.out.println("py 좌표 : " + py + " px 좌표 : "+ px);
-			visited[py][px] = true;
-			
-			for(int i=0; i<4; i++) {
-				py += dy[i];
-				px += dx[i];
-				if(py>=0&&px>=0&&py<N&&px<M&&!visited[py][px]&&map[py][px]==0) {
-					queue.add(py);
-					queue.add(px);
-					queue.add(depth+1);//깊이로 몇일차인지 파악한다.
-					visited[py][px] = true;
-					map[py][px] = 1;
+	public static boolean visitedFail() {
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<M; j++) {
+				if(!visited[i][j]&&(map[i][j]!=-1)) {
+					return true;
 				}
-				py -= dy[i];
-				px -= dx[i];
 			}
-			
 		}
+		return false;
 	}
 }
