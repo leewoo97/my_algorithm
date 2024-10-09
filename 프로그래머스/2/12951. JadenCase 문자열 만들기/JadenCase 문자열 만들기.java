661af54fff4208350d7fe60692478058753bdae3
Woo Jin-Lee <138551127+leewoo97@users.noreply.github.com>
2024-10-09 09:33:43 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 3.30 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String solution(String s) {
+        String result="";
+        String[] arr=s.split(" ");
+        
+        for(int i=0; i<arr.length; i++){
+            if(arr[i].length()==0){
+                result+=" ";            
+            }else{
+                result+=arr[i].substring(0,1).toUpperCase();
+                result+=arr[i].substring(1,arr[i].length()).toLowerCase();
+                result+=" ";
+            }
+            
+            // if(i!=arr.length-1)
+            //     result+=" ";
+        }
+        
+        if(s.substring(s.length()-1, s.length()).equals(" ")){
+            return result;
+        }
+        
+        // 맨 마지막 " " 제거 후 반환
+        return result.substring(0, result.length()-1);
+        
+        
+        // String answer = result;
+        // return answer;
+    }
+}
