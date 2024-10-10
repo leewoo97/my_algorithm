0e1600e257c1a6b12ab8ef2b2de4da206552121d
Woo Jin-Lee <138551127+leewoo97@users.noreply.github.com>
2024-10-10 13:44:06 +0900

    [level 3] Title: 최고의 집합, Time: 1.74 ms, Memory: 52.8 MB -BaekjoonHub

---
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+  public int[] solution(int n, int s) {
+      if(n > s)
+          return new int[]{-1};
+      
+      int[] answer = new int[n];
+      for(int i=0; i<answer.length; i++)
+          answer[i] = s/n;
+      
+      int temp=s%n;
+      for(int i=0; i<temp; i++)
+          answer[i] ++;
+      
+      Arrays.sort(answer);
+      
+      return answer;
+  }
+}
